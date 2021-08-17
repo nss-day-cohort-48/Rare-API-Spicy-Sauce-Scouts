b28d25dfa5026f058f52e9cdd89a0d6a1e799be7
shawnbarron16 <shawnbarron16@gmail.com>
2021-08-17 08:23:04 -0500

    post model created

---
+++ rareapi/models/post.py
@@ -0,0 +1,24 @@
+from django import models
+
+class Post(models.Model):
+    """Post Model
+    Fields:
+        title (CharField): title of the post
+        category (ForeignKey): id of the category the post is in
+        content (CharField): The post caption
+        publication_date (Date): Date the post was published
+        image_url (CharField): url for the post image
+        user (ForeignKey): id of the author of the post
+        approved (Boolean): if approved or not by admin
+    """
+
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
+    publication_date = models.DateField.auto_now
+    image_url = models.CharField(max_length=500)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    approved = models.BooleanField()
+
+def __str__(self):
+    return self.name
